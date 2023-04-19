964e6f0c4182479da8e491f6b83190f1f9f6be4e
Agbera <126923646+Agbera@users.noreply.github.com>
2023-04-19 19:45:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def hello_firestore(event, context):
     # now print out the entire event object
     print(str(event))
 #projects/avian-amulet-378416/topics/func-1-demo
-    topic_name = 'pub-demosample'
+    topic_name = 'pud-demosample'
     PROJECT_ID =  'my-project-1-383207'
     message = str(event)
 
